f8568b11ab8ea1b3f725412f9bdef40fc72d5b82
LeticiaLuckow <leticia.villasboas99@gmail.com>
2022-12-27 11:24:19 -0300

    fix-mocks

--- src/pages/store/index.tsx
+++ src/pages/store/index.tsx
@@ -5,7 +5,7 @@ import Card from '../../components/card';
 import Header from '../../components/header';
 import { ObjectProduct } from '../../types/product';
 import { CardStore, CardStoreP } from '../../styles/store/styles';
-import { PRODUCTS_MOCK } from './products-mock';
+import { PRODUCTS_MOCK } from '../../mocks/products-mock';
 
 const Store: NextPage = () => {
   const [product, setProduct] = useState<ObjectProduct[]>();
